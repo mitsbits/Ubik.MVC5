9d049538186ea81ee02b9eb92db123396bfcb503
mitsbits <mitsbits@gmail.com>
2015-10-03 00:50:47 +0300

    script onjection backoffice

---
+++ Ubik.UI.MVC/Areas/Backoffice/Scripts/framework/UserAdministration/Roles.js
@@ -0,0 +1,30 @@
+﻿$(function () {
+    $('#copy-role-modal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var data = button.data('source');
+        var modal = $(this);
+        modal.find('span#roleTitleName').text(data);
+        modal.find('input#Name').val(data);
+        modal.find('input#Target').val('');
+    });
+
+    $("#roles-table").dataTable({
+        "bPaginate": true,
+        "bLengthChange": false,
+        "bFilter": false,
+        "bSort": false,
+        "bInfo": false,
+        "bAutoWidth": true
+    });
+
+    jQuery('#copy-role-form').validate();
+    // Add a custom class to your name mangled input and add rules like this
+    jQuery('#Target').rules('add', {
+        required: true,
+        messages: {
+            required: 'Some custom message for the username required field'
+        }
+    });
+
+
+});
--- Ubik.Web.Backoffice/BackofficePage.cs
+++ Ubik.Web.Backoffice/BackofficePage.cs
@@ -1,25 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
+using System.Web.Hosting;
 using System.Web.Mvc;
 using Ubik.Web.Backoffice;
+using Ubik.Web;
 
 namespace Ubik.Web.Backoffice
 {
-    public abstract class BackofficePage : WebViewPage
-    {
-        protected BackofficePage()
-            : base()
-        {
-        }
-        public BackofficeContentHelper PageContent { get; private set; }
-        public override void InitHelpers()
-        {
-            base.InitHelpers();
-            PageContent = new BackofficeContentHelper(ViewContext);
-        }
-    }
+    //public abstract class BackofficePage : WebViewPage
+    //{
+    //    protected BackofficePage()
+    //        : base()
+    //    {
+    //    }
+    //    public BackofficeContentHelper PageContent { get; private set; }
+    //    public override void InitHelpers()
+    //    {
+    //        base.InitHelpers();
+    //        PageContent = new BackofficeContentHelper(ViewContext);
+    //    }
+
+
+    //}
 
     public abstract class BackofficePage<TModel> : WebViewPage<TModel>
     {
@@ -33,6 +38,13 @@ namespace Ubik.Web.Backoffice
         {
             base.InitHelpers();
             PageContent = new BackofficeContentHelper(ViewContext);
+        }
+        public void AddBackofficeBottom(string url)
+        {
+            const string prefix = @"~/Areas/Backoffice/Scripts/framework/";
+            url = url.TrimStart('/');
+            var path = string.Format("{0}{1}", Url.Content(prefix), (url.EndsWith(".js") ? url : url + ".js"));
+            Html.Statics().FooterScripts.Add(path);
         }
     }
 }
