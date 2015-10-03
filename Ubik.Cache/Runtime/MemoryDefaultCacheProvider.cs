19ab691e6f067a3d69768dfa6e0e7dbb3c7379e4
mitsbits <mitsbits@gmail.com>
2015-10-04 02:52:41 +0300

    cache

---
+++ Ubik.Cache/Runtime/MemoryDefaultCacheProvider.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Caching;
+using Ubik.Infra.Contracts;
+
+namespace Ubik.Cache.Runtime
+{
+    public class MemoryDefaultCacheProvider : ICacheProvider
+    {
+        protected static MemoryCache _cache;
+        protected Object _lock = new Object();
+
+        protected virtual MemoryCache CurrentCache
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_cache == null)
+                        _cache = MemoryCache.Default;
+                }
+                return _cache;
+            }
+        }
+
+        public virtual object GetItem(string key)
+        {
+            return CurrentCache[key.ToLowerInvariant()];
+        }
+
+        public virtual void SetItem(string key, object value)
+        {
+            if (value == null) return;
+            lock (_lock)
+            {
+                var policy = new CacheItemPolicy
+                {
+                    SlidingExpiration = ObjectCache.NoSlidingExpiration,
+                    AbsoluteExpiration = ObjectCache.InfiniteAbsoluteExpiration
+                };
+                CurrentCache.Set(key.ToLowerInvariant(), value, policy);
+            }
+        }
+
+        public virtual void SetItem(string key, object value, params int[] ttl)
+        {
+            if (value == null) return;
+            var ttlCount = (ttl.Count() > 4) ? 4 : ttl.Count();
+            var cacheDur = DateTimeOffset.Now;
+            for (var i = 0; i < ttlCount; i++)
+            {
+                switch (i)
+                {
+                    case 3:
+                        cacheDur = cacheDur.AddSeconds(ttl[3]);
+                        break;
+
+                    case 2:
+                        cacheDur = cacheDur.AddMinutes(ttl[2]);
+                        break;
+
+                    case 1:
+                        cacheDur = cacheDur.AddHours(ttl[1]);
+                        break;
+
+                    case 0:
+                        cacheDur = cacheDur.AddDays(ttl[0]);
+                        break;
+                }
+            }
+
+            lock (_lock)
+            {
+                var policy = new CacheItemPolicy
+                {
+                    SlidingExpiration = ObjectCache.NoSlidingExpiration,
+                    AbsoluteExpiration = cacheDur
+                };
+                CurrentCache.Set(key.ToLower(), value, policy);
+            }
+        }
+
+        public virtual void SetItem(string key, object value, DateTime absoluteExpiration)
+        {
+            if (value == null) return;
+            lock (_lock)
+            {
+                var policy = new CacheItemPolicy
+                {
+                    SlidingExpiration = ObjectCache.NoSlidingExpiration,
+                    AbsoluteExpiration = absoluteExpiration
+                };
+                CurrentCache.Set(key.ToLower(), value, policy);
+            }
+        }
+
+        public virtual void RemoveItem(string key)
+        {
+            if (!CurrentCache.Contains(key.ToLowerInvariant())) return;
+            lock (_lock)
+            {
+                CurrentCache.Remove(key.ToLower());
+            }
+        }
+    }
+}
---
+++ Ubik.Cache/Runtime/MemoryNamedCacheProvider.cs
@@ -0,0 +1,32 @@
+using System.Collections.Specialized;
+using System.Runtime.Caching;
+
+namespace Ubik.Cache.Runtime
+{
+    public class MemoryNamedCacheProvider : MemoryDefaultCacheProvider
+    {
+        private readonly string _name;
+        private readonly NameValueCollection _config;
+
+        public MemoryNamedCacheProvider(string name, NameValueCollection config = null)
+            : base()
+        {
+            //TODO: validate input
+            _name = name;
+            _config = config;
+        }
+
+        protected override MemoryCache CurrentCache
+        {
+            get
+            {
+                lock (_lock)
+                {
+                    if (_cache == null)
+                        _cache = new MemoryCache(_name, _config);
+                }
+                return _cache;
+            }
+        }
+    }
+}
