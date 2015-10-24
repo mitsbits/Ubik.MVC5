3ca1f50ee0f2505ff0481c569554b9e0462ef681
mitsbits <mitsbits@gmail.com>
2015-10-24 15:04:56 +0300

    slugirier for https://github.com/mitsbits/Ubik/issues/28

--- Ubik.Tests.Web.Infra/SystemSlugifierTest.cs
+++ Ubik.Tests.Web.Infra/SystemSlugifierTest.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using Shouldly;
+using System.Collections.Generic;
 using Ubik.Web.Infra.Contracts;
 using Ubik.Web.Infra.Services;
 
 namespace Ubik.Tests.Web.Infra
 {
-     [TestFixture]
+    [TestFixture]
     public class SystemSlugifierTest
     {
-        private  ISlugifier _slugifier;
+        private ISlugifier _slugifier;
+
         [SetUp]
         public void SetUp()
         {
-            var asciis = new List<IInternationalCharToAsciiProvider>(new[] {new GreekToAsciiProvider(),});
-            var worders = new List<ISlugWordReplacer>(new[] {new SystemSlugWordRplacer(),});
-            var ommiters = new List<ISlugCharOmmiter>(new[] {new SystemSlugCharReplacer(),});
+            var asciis = new List<IInternationalCharToAsciiProvider>(new[] { new GreekToAsciiProvider(), });
+            var worders = new List<ISlugWordReplacer>(new[] { new SystemSlugWordRplacer(), });
+            var ommiters = new List<ISlugCharOmmiter>(new[] { new SystemSlugCharReplacer(), });
             _slugifier = new SystemSlugService(worders, ommiters, asciis);
         }
-
 
         [TestCase("Hello vanilla face!", "hello-vanilla-face")]
         [TestCase("No shit!@#@!", "no-shit")]
@@ -30,6 +28,23 @@ namespace Ubik.Tests.Web.Infra
             _slugifier.SlugifyText(source).ShouldBe(result);
         }
 
+        [TestCase("Hello        vanilla                 face!", "hello-vanilla-face")]
+        [TestCase("No        shit           !@#@!", "no-shit")]
+        public void test_that_repeated_spaces_are_removed(string source, string result)
+        {
+            _slugifier.ShouldNotBeNull();
+            _slugifier.SlugifyText(source).ShouldBe(result);
+        }
 
+        [TestCase("!@#$", "")]
+        [TestCase("...", "")]
+        [TestCase("~!)(*&^%$#@", "")]
+        [TestCase("-", "")]
+        [TestCase("_-=+/", "")]
+        public void test_that_invalid_inputs_produce_empty_string(string source, string result)
+        {
+            _slugifier.ShouldNotBeNull();
+            _slugifier.SlugifyText(source).ShouldBe(result);
+        }
     }
 }
