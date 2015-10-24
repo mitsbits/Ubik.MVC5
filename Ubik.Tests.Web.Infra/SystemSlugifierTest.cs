ea4da09f756ea361971ef52d1f56510efd557a00
mitsbits <mitsbits@gmail.com>
2015-10-24 14:23:37 +0300

    started https://github.com/mitsbits/Ubik/issues/28

---
+++ Ubik.Tests.Web.Infra/SystemSlugifierTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+using Ubik.Web.Infra.Contracts;
+using Ubik.Web.Infra.Services;
+
+namespace Ubik.Tests.Web.Infra
+{
+    [TestClass]
+    public class SystemSlugifierTest
+    {
+        private  ISlugifier _slugifier;
+        [TestInitialize]
+        public void SetUp()
+        {
+            var asciis = new List<IInternationalCharToAsciiProvider>(new[] {new GreekToAsciiProvider(),});
+            var worders = new List<ISlugWordReplacer>(new[] {new SystemSlugWordRplacer(),});
+            var ommiters = new List<ISlugCharOmmiter>(new[] {new SystemSlugCharReplacer(),});
+            _slugifier = new SystemSlugService(worders, ommiters, asciis);
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            _slugifier.ShouldNotBeNull();
+            _slugifier.SlugifyText("Hello vanilla face!").ShouldBe("hello-vanilla-face");
+        }
+
+
+    }
+}
