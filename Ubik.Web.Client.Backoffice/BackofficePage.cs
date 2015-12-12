66b4d1b941e26c5e809a17419026925b574a7893
mitsbits <mitsbits@gmail.com>
2015-12-12 02:47:35 +0200

    Added admin lte to backoffice layout - https://github.com/mitsbits/Ubik/issues/30

--- Ubik.Web.Client.Backoffice/BackofficePage.cs
+++ Ubik.Web.Client.Backoffice/BackofficePage.cs
@@ -5,6 +5,60 @@ using Ubik.Web.Basis.Helpers;
 
 namespace Ubik.Web.Client.Backoffice
 {
+    public abstract class BackofficePage : RazorPage
+    {
+            
+        protected BackofficePage()
+            : base()
+        {
+
+        }
+
+
+
+        private BackofficeContentHelper _pageContent;
+        public BackofficeContentHelper PageContent
+        {
+            get
+            {
+                if (_pageContent == null) _pageContent = new BackofficeContentHelper(ViewContext);
+                return _pageContent;
+
+            }
+        }
+        private ServerResponseHelper _feedback;
+        public ServerResponseHelper Feedback
+        {
+            get
+            {
+                if (_feedback == null) _feedback = new ServerResponseHelper(ViewContext);
+                return _feedback;
+
+            }
+        }
+
+        public void AddBackofficeBottom(string urlstring, IUrlHelper url, IHtmlHelper html)
+        {
+            const string prefix = @"~/Areas/Backoffice/Scripts/framework/";
+            urlstring = urlstring.TrimStart('/');
+
+            var path = string.Format("{0}{1}", url.Content(prefix), (urlstring.EndsWith(".js") ? urlstring : urlstring + ".js"));
+            html.Statics().FooterScripts.Add(path);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public abstract class BackofficePage<TModel> : RazorPage<TModel>
     {
---
+++ Ubik.Web.Client.Backoffice/ViewComponents/MainMenuComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Ubik.Web.BuildingBlocks.Contracts;
+
+namespace Ubik.Web.Client.Backoffice.ViewComponents
+{
+    [ViewComponent(Name = "MainMenu")]
+    public class MainMenuComponent : ViewComponent
+    {
+        private readonly IResident _resident;
+        public MainMenuComponent(IResident resident)
+        {
+            _resident = resident;
+        }
+
+        public  IViewComponentResult  Invoke()
+        {
+            var model = _resident.Administration.BackofficeMenu;
+            return View(model);
+        }
+    }
+}
